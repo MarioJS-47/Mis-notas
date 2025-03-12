f26ccfef6cd4b7ebc020d95b0732ec2586cb12f2
Mario Juarez <154840111+MarioJS-47@users.noreply.github.com>
2025-03-12 00:39:12 -0600

    Update Sentencias SQL.sql

--- Sentencias SQL.sql
+++ Sentencias SQL.sql
@@ -1,66 +1,67 @@
 -- Crear base de datos
-CREATE DATABASE pedidos;
+CREATE DATABASE nombre_database;
 
 -- Usar base de datos
-USE pedidos; 
+USE nombre_database; 
 
 --Crear tabla
-CREATE TABLE refacciones(    
-    codigo INT PRIMARY KEY,  
-    descripcion VARCHAR(200),
-    categoria VARCHAR(20),   
-    precio DECIMAL(10,2) -- Cantidad de cifras enteras / Cantidad de cifras decimales
-);
+CREATE TABLE nombre_tabla(    
+    columna_numero INT,  
+    columna_texto1 VARCHAR(200),
+    columna_texto2 VARCHAR(20),   
+    columna_decimal DECIMAL(10,2), -- Cantidad de cifras enteras / Cantidad de cifras decimales
+    PRIMARY KEY(columna_numero)
+) ENGINE = InnoDB; -- Motor de almacenamiento habilitado para uso de llaves foraneas
 
 -- Renombrar tabla
-RENAME TABLE pedidos TO orders;
+RENAME TABLE nombre_tabla TO nuevo_nombre;
 
 -- Inserccion de datos                      
-INSERT INTO refacciones (codigo, descripcion, categoria, precio) VALUES (40061762, 'Cable ethernet RJ45 a RJ45 - 10m', 'electrica', 250.20);                                      
+INSERT INTO nombre_tabla (columna1, columna2, columna3, columna4) VALUES (40061762, 'Cable ethernet RJ45 a RJ45 - 10m', 'electrica', 250.20);                                      
                                                                                                         
 -- Consulta de datos                                                                                           
-SELECT * FROM refacciones;                                                                                     
+SELECT * FROM nombre_tabla;                                                                                     
                                                                                                                
 -- Consulta de columnas especificas                                                                            
-SELECT codigo, precio FROM refacciones;                                                                        
+SELECT columna1, columna2 FROM nombre_tabla;                                                                        
                                                                                                                
 -- Consulta con alias                                                                                          
-SELECT codigo AS Mi_codigo FROM refacciones;                                                                   
+SELECT nombre_columna AS alias_columna FROM nombre_tabla;                                                                   
                                                                                                                
 -- Consulta con criterio                                                                                       
-SELECT * FROM refacciones WHERE categoria = 'electrica';                                                       
+SELECT * FROM nombre_tabla WHERE nombre_columna = 'criterio';                                                       
                                                                                                                
 -- Consulta con doble criterio                                                                                 
-SELECT * FROM refacciones WHERE categoria IN ('electrica','neumatica');                                        
+SELECT * FROM nombre_tabla WHERE nombre_columna IN ('criterio1','criterio2');                                        
                                                                                                                
 -- Consulta con exepcion                                                                                       
-SELECT * FROM refacciones WHERE NOT categoria = 'neumatica';                                                   
+SELECT * FROM nombre_tabla WHERE NOT nombre_columna = 'criterio';                                                   
                                                                                                                
 -- Consulta LIKE para buscar registros que contengan palabra en cualquier lugar del registro                   
-SELECT * FROM refacciones WHERE descripcion LIKE '%ethernet%';                                                 
+SELECT * FROM nombre_tabla WHERE nombre_columna LIKE '%criterio%';                                                 
                                                                                                                
 -- Consulta LIKE para buscar registros que terminen con la palabra indicada                                    
-SELECT * FROM refacciones WHERE descripcion LIKE '%ethern';
+SELECT * FROM nombre_tabla WHERE nombre_columna LIKE '%criterio';
 
 -- Realizar consulta de registros unicos descartando repetidos
-SELECT DISTINCT * FROM refacciones;
+SELECT DISTINCT * FROM nombre_tabla;
 
 -- Consultar todo de tabla limitando a 4 primeros registros
-SELECT * FROM refacciones LIMIT 4;
+SELECT * FROM nombre_tabla LIMIT 4;
 
 -- Consultar listado limitado a solo visualizar de registro 4 al 10
-SELECT * FROM refacciones LIMIT 4,10;
+SELECT * FROM nombre_tabla LIMIT 4,10;
 
 -- Consulta agrupacion de datos basado en COUNT (Conteo), es posible aplicar operaciones con MIN, MAX, AVG, SUM
-SELECT categoria, COUNT(precio) FROM  refacciones
-GROUP BY categoria;
+SELECT nombre_columna, COUNT(nombre_columna2) FROM  nombre_tabla
+GROUP BY nombre_columna;
 
 -- Aplicar operacion total de la tabla
-SELECT SUM(precio) FROM refacciones;
+SELECT SUM(nombre_columna) FROM nombre_tabla;
 
 -- Consulta de total por GROUP BY
-SELECT categoria, count(*) FROM refacciones
-GROUP BY categoria;
+SELECT nombre_columna, count(*) FROM nombre_tabla
+GROUP BY nombre_columna2;
 
 -- Consulta suma de precios por categoria de refacciones, agrupando por categoria, teniendo que la suma de precios sea mayor a 100
 SELECT categoria, SUM(precio)AS precio_categoria FROM  refacciones
